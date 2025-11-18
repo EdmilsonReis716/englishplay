88683320fca12c713231bd4336f1add219950e32
Edmilson <aprendizvanderlande@gmail.com>
2025-11-18 11:24:16 -0300

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,4 +0,0 @@
-// firebase-init.js - paste your Firebase config here
-// Example:
-// var firebaseConfig = { apiKey: '...', authDomain: '...', projectId: '...', storageBucket: '...', messagingSenderId: '...', appId: '...' };
-console.log('Add your Firebase config to firebase-init.js if you want Firebase integration');
